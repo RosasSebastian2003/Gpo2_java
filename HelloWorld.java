6bb7e56a9c7845e494f33c98fff0c05f036f53f2
LuisArellanoMX <le.arellanohernandez@ugto.mx>
2023-09-07 17:51:34 -0600

    By Luis Ernesto Arellano

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hola Mundo");
         System.out.println("Esta es otra prueba");
+        System.out.println("Linea escrita por Luis Ernesto Arellano");
     }
 }
