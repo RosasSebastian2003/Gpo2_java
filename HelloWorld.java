c6cb3fde8a1f4bd4cc081dc69de538d8f63b576e
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-09-07 16:49:49 -0600

    version 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hola Mundo");
         System.out.println("Este es un segundo commit");
+        
+        String a, b;
+
+        a = "Este es un string";
+        b = "Este es otro string";
     }
 }
